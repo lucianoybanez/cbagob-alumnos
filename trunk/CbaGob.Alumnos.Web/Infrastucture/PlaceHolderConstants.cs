a17d268862b85aeb2555c3e781a79c340819b120
lucianoybanez <lucianoybanez@5c3138a1-1043-5024-28a4-1b850aa844c6>
2012-05-19 17:54:45 +0000

    add PlaceHolderConstants and placeholder attribute, and add entrar con el enter

---
+++ trunk/CbaGob.Alumnos.Web/Infrastucture/PlaceHolderConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace CbaGob.Alumnos.Web.Infrastucture
+{
+    public static class PlaceHolderConstants
+    {
+        public const string InstitucionNombre = "ej. Corazón de María";
+        public const string InstitucionTelefono = "ej. 0351-155555555";
+        public const string InstitucionProvincia = "ej. Córdoba";
+        public const string InstitucionLocalidad = "ej. Capital";
+        public const string InstitucionBarrio = "ej. Nueva Córdoba";
+        public const string InstitucionNumero = "ej. 845";
+        public const string InstitucionDepartamento = "ej. 8 'A'";
+        public const string Resolucion = "ej. 85-45847";
+        public const string Expediente = "ej. 8444485";
+        public const string SedeNombre = "ej. Central";
+        public const string CursoNombre = "ej. Programación";
+    }
+}
